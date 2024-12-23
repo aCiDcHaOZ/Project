796dfbcd787c2234fdee8e020a286f33e6c2bc2a
Acidchaoz <redemption.is.yours@gmail.com>
2024-12-23 14:46:19 +0100

    regform laden aub

--- form_aanmelden.py
+++ form_aanmelden.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mijngeheimesleutel'
@@ -9,14 +10,14 @@ class InfoForm(FlaskForm):
     instrument = StringField('Welk instrument wil je graag bespelen?')
     submit = SubmitField('Verzend')
 
-@app.route('/', methods=['GET', 'POST']) #Route defineren voor het formulier
+@app.route("/", methods=['GET', 'POST']) #Route defineren voor het formulier
 def index():
     instrument = False 
     form = InfoForm() #maak een object van het type InfoForm()
     if form.validate_on_submit():
         instrument = form.instrument.data
         form.instrument.data = ''
-        return render_template('home.html', form=form, instrument=instrument)
+        return render_template('/templates/registreren.html', form=form, instrument=instrument)
     
 if __name__ == '__main__':
     app.run(debug=True)
