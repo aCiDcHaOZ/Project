980476477661746b049a51304bf93bf853e4ee75
Brenda <brendaverboekend@hotmail.com>
2024-12-23 15:01:54 +0100

    boodschaP

--- form_aanmelden.py
+++ form_aanmelden.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mijngeheimesleutel'
 
 class InfoForm(FlaskForm):
-    instrument = StringField('Welk instrument wil je graag bespelen?')
+    instrument = StringField('Welk instrument wil je graag bespelen?', validators=[DataRequired()])
     submit = SubmitField('Verzend')
 
-@app.route("/", methods=['GET', 'POST']) #Route defineren voor het formulier
+@app.route("/", methods=['GET', 'POST'])  # Route defineren voor het formulier
 def index():
-    instrument = False 
-    form = InfoForm() #maak een object van het type InfoForm()
+    instrument = None  # Initialiseer instrument als None
+    form = InfoForm()  # Maak een object van het type InfoForm
     if form.validate_on_submit():
         instrument = form.instrument.data
-        form.instrument.data = ''
-        return render_template('/templates/registreren.html', form=form, instrument=instrument)
-    
+        form.instrument.data = ''  # Reset het invoerveld
+    return render_template('registreren.html', form=form, instrument=instrument)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
